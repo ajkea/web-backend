e63c9b93d90ab20c2ae0b8208de0333772fbb306
Ajkea <hendrik.vandijck@student.kdg.be>
2017-08-10 15:02:43 +0200

    add changes logon message

--- herexamen1/resources/views/home.blade.php
+++ herexamen1/resources/views/home.blade.php
@@ -2,11 +2,13 @@
 @section('content')
             <div class="container">
             @auth
+                <div class="alert alert-success fade in">
+                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                    Goed gedaan, je hebt je kunnen aanmelden!
+                </div>
             <h1>Herexamen web back-end</h1>
             <h2>Hendrik Van Dijck</h2>
-            <p class="btn btn-success">
-                Goed gedaan, je hebt je kunnen aanmelden!
-            </p>
+            
             @endauth
             </div>
         </div>
